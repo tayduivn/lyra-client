bf645f8622b8a67e8a716c616e53f9cb8e9dc035
Luke Hamilton <lukehamiltonmail@gmail.com>
2018-10-11 09:38:15 -0400

    pagination

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import App from '../components/App';
 import Header from '../components/Header';
 import Submit from '../components/Submit';
@@ -7,8 +8,8 @@ import {Query} from 'react-apollo';
 import gql from 'graphql-tag';
 
 const productsQuery = gql`
-  query products($first: Int!) {
-    products(first: $first) {
+  query products($first: Int!, $skip: Int!) {
+    products(first: $first, skip: $skip) {
       id
       name
       slug
@@ -24,7 +25,8 @@ const productsQuery = gql`
 `;
 
 export const productsQueryVars = {
-  first: 5
+  first: 5,
+  skip: 0
 };
 
 export default () => (
@@ -36,18 +38,39 @@ export default () => (
       {({loading, error, data: {products, _productsMeta}, fetchMore}) => {
         if (loading) return <div>Loading</div>;
         // console.log(error);
-        const product = products[0];
         console.log('products', products);
         return (
           <div>
             <pre>{products.toString()}</pre>
-            <ProductCard
-              key={product.id}
-              name={product.name}
-              description={product.description}
-              imageUrl={product.imageUrl}
-              tags={product.topics}
-            />
+            {products.map(product => {
+              return (
+                <ProductCard
+                  key={product.id}
+                  name={product.name}
+                  description={product.description}
+                  imageUrl={product.imageUrl}
+                  tags={product.topics}
+                />
+              );
+            })}
+            <a
+              onClick={() =>
+                fetchMore({
+                  variables: {
+                    skip: products.length,
+                    first: 5
+                  },
+                  updateQuery: (prev, {fetchMoreResult}) => {
+                    if (!fetchMoreResult) return prev;
+                    return Object.assign({}, prev, {
+                      products: [...prev.products, ...fetchMoreResult.products]
+                    });
+                  }
+                })
+              }
+            >
+              Load More
+            </a>
           </div>
         );
         // return <pre>{products.toString()}</pre>;
@@ -55,3 +78,46 @@ export default () => (
     </Query>
   </App>
 );
+
+const FEED_QUERY = gql`
+  query Feed($type: FeedType!, $offset: Int, $limit: Int) {
+    currentUser {
+      login
+    }
+    feed(type: $type, offset: $offset, limit: $limit) {
+      id
+      # ...
+    }
+  }
+`;
+//
+// const FeedData = ({ match }) => (
+//   <Query
+//     query={FEED_QUERY}
+//     variables={{
+//       type: match.params.type.toUpperCase() || "TOP",
+//       offset: 0,
+//       limit: 10
+//     }}
+//     fetchPolicy="cache-and-network"
+//   >
+//     {({ data, fetchMore }) => (
+//       <Feed
+//         entries={data.feed || []}
+//         onLoadMore={() =>
+//           fetchMore({
+//             variables: {
+//               offset: data.feed.length
+//             },
+//             updateQuery: (prev, { fetchMoreResult }) => {
+//               if (!fetchMoreResult) return prev;
+//               return Object.assign({}, prev, {
+//                 feed: [...prev.feed, ...fetchMoreResult.feed]
+//               });
+//             }
+//           })
+//         }
+//       />
+//     )}
+//   </Query>
+// );
