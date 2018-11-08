468a5ed67c30bf3352789002267f94d92ce06149
Luke Hamilton <lukehamiltonmail@gmail.com>
2018-11-07 23:48:42 -0500

    WIP

--- components/Header.js
+++ components/Header.js
@@ -2,19 +2,29 @@ import styled from 'react-emotion';
 import Link from 'next/link';
 import {withRouter} from 'next/router';
 
-const Container = styled.header`
-  background-color: ${props => props.theme.black};
+const Wrapper = styled.header`
+  background-color: ${props => props.theme.white};
+  height: 66px;
+  border-bottom: 1px solid ${props => props.theme.lilac};
+`;
+
+const Container = styled.div`
+  max-width: 1100px;
+  display: flex;
+  margin: auto;
 `;
 
 const Header = ({router: {pathname}}) => (
-  <Container>
-    <Link prefetch href="/">
-      <a className={pathname === '/' ? 'is-active' : ''}>Home</a>
-    </Link>
-    <Link prefetch href="/about">
-      <a className={pathname === '/about' ? 'is-active' : ''}>About</a>
-    </Link>
-  </Container>
+  <Wrapper>
+    <Container>
+      <Link prefetch href="/">
+        <a className={pathname === '/' ? 'is-active' : ''}>Home</a>
+      </Link>
+      <Link prefetch href="/about">
+        <a className={pathname === '/about' ? 'is-active' : ''}>About</a>
+      </Link>
+    </Container>
+  </Wrapper>
 );
 
 // const Header = ({router: {pathname}}) => (
