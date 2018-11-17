59026f913c69320447260103a2e189f7d841ac14
Luke Hamilton <lukehamiltonmail@gmail.com>
2018-11-16 23:41:45 -0500

    WIP

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,23 @@ import App, {Container} from 'next/app';
 import Page from '../components/Page';
 import withApolloClient from '../lib/with-apollo-client';
 import {ApolloProvider} from 'react-apollo';
+import Auth from '../lib/auth/auth';
 
 class MyApp extends App {
+  componentDidMount() {
+    // const isClient = typeof window !== 'undefined';
+    // if (isClient) {
+    //   debugger;
+    // }
+  }
+
   render() {
+    // const isClient = typeof window !== 'undefined';
+    // if (isClient) {
+    //   debugger;
+    // }
+    const auth = new Auth(result => console.log('auth result', result), this.props.apolloClient);
+    console.log('component did mount!', auth);
     const {Component, pageProps, apolloClient} = this.props;
     return (
       <Container>
